dca30897beab908af1cf1cd6338fbb4c79a62dca
mihhail-m <mihhail.matisinets@gmail.com>
2025-04-12 22:54:57 +0300

    base spec file for tests

--- src/test/groovy/BaseSpecification.groovy
+++ src/test/groovy/BaseSpecification.groovy
@@ -1,3 +1,16 @@
+import io.restassured.RestAssured
+import spock.lang.Specification
 
-class BaseSpecification {
+
+/*
+ * All common config for spec files goes here
+ */
+abstract class BaseSpecification extends Specification {
+    def setupSpec() {
+        RestAssured.baseURI = "http://localhost"
+        RestAssured.port = 8080
+        RestAssured.basePath = "/api/person"
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+    }
 }
